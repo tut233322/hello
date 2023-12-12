0cadd7e81ad2affeee0f3ddcfeb684eaa75b5742
tut233322 <153588162+tut233322@users.noreply.github.com>
2023-12-12 16:03:50 +0900

    Update Hello.java
    fix typo

--- Hello.java
+++ Hello.java
@@ -1,3 +1,3 @@
 public class Hello {
-  public static void main(Strint[] args) { System.out.println("hello world"); }
+  public static void main(String[] args) { System.out.println("hello world"); }
 }
